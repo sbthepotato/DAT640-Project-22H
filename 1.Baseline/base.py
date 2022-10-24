a166c8268c29d4d47ff5cb495791acacb8e35c42
Stephan Brandasu <Stephanbrandasu@gmail.com>
2022-10-24 12:47:02 +0200

    it works, just need to run it

--- 1.Baseline/base.py
+++ 1.Baseline/base.py
@@ -10,16 +10,22 @@ if __name__=="__main__":
         i['question'] = fun.preprocess(str(i['question']))
     print('finished preprocess train')
 
-    for i in dbpedia_questions:
-        i['question'] = fun.preprocess(str(i['question']))
-    print('finished preprocess questions')
-
     num_questions = len(dbpedia_questions)
+    # quickly comment one or the other in/out to test program on reduced dataset
+    #for i, j in enumerate(dbpedia_questions[:10]):
     for i, j in enumerate(dbpedia_questions):
-        category, type = fun.answerQuery(j['question'], dbpedia_train)
-        if (i + 1) % (num_questions // 100) == 0:
-            print(f"{round(100*(i/num_questions))}% answered.")
+        question_processed = fun.preprocess(str(j['question']))
+        category, type = fun.answerQuery(question_processed, dbpedia_train)
+        # prints out the progress of question answering, taken from A2.1
+        try:
+            if (i + 1) % (num_questions // 100) == 0:
+                print(f"{round(100*(i/num_questions))}% answered.")
+        except:
+            print('something went wrong with the progress printout')
+        dbpedia_questions[i]['category'] = category
+        dbpedia_questions[i]['type'] = type
 
+    print('finished answering dataset')
     # writes the dict with answers into the same directory with the test and train files
     fun.writeData('../datasets/DBpedia/smarttask_dbpedia_test_answers.json', dbpedia_questions)
 
--- 1.Baseline/base_functions.py
+++ 1.Baseline/base_functions.py
@@ -26,7 +26,7 @@ def writeData(filelocation, dictionary):
     """Takes a filelocation and a dictionary and turns it into a json file
         have to make sure when making the dict that its the correct format.
     """
-    json_object = json.dumps(dictionary, indent=4)
+    json_object = json.dumps(dictionary, indent=2)
     # could also do this but the current one is more readable
     # json.dump(dictionary, file)
     with open(filelocation, "w") as file:
@@ -35,18 +35,25 @@ def writeData(filelocation, dictionary):
 def answerQuery(query, train):
     """Takes a question and the training dataset and finds the highest score match from the training dataset
     """
-    bm25 = BM25Okapi(query)
+    try:
+        bm25 = BM25Okapi(query)
+    except ZeroDivisionError as e:
+            print('Zero division while creating instance')
+            print(e)
+            return 'boolean', 'boolean'
     score = 0
     categoryA = ''
     typeA = ''
     for j in train:
-        ques = j['question']
-        scores = bm25.get_scores(ques)
-        if scores[0] > score:
-            score = scores[0]
-            categoryA = j['category']
-            typeA = j['type']
-       
+        try:
+            scores = bm25.get_scores(j['question'])
+            if scores[0] > score:
+                score = scores[0]
+                categoryA = j['category']
+                typeA = j['type']
+        except ZeroDivisionError as e:
+            print('Zero division while scoring')
+            print(e)
     return categoryA, typeA
 
 
